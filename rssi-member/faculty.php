5099308144001e478c7846bb0c1d2c0bf6ae98d9
Somnath <sahasomnath93@gmail.com>
2021-05-29 23:29:16 +0530

    ok

---
+++ rssi-member/faculty.php
@@ -0,0 +1,77 @@
+<?php
+session_start();
+// Storing Session
+$user_check = $_SESSION['aid_admin'];
+
+if (!$_SESSION['aid_admin']) {
+
+  header("Location: index_admin.php"); //redirect to the login page to secure the welcome page without login access.
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <title>Admin Corner</title>
+  <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon" />
+  <!-- Main css -->
+  <link rel="stylesheet" href="../rssi-student/style.css">
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <!------ Include the above in your HEAD tag ---------->
+
+</head>
+
+<?php
+include("database.php");
+$result = pg_query($con, "SELECT * FROM memberdata order by filterstatus asc,today desc");
+if (!$result) {
+  echo "An error occurred.\n";
+  exit;
+}
+
+$resultArr = pg_fetch_all($result);
+//print_r($resultArr);
+
+echo '<table class="table">
+<thead>
+    <tr>
+         <th>Photo</th>
+         <th>Volunteer Details</th>
+         <th>Designation</th>
+         <th>Class Allotment</th>
+         <th>Class URL</th>
+         <th>Association Status</th>
+         <th>Timesheet defaulters</th>
+         <th>Password</th>
+         <th>Productivity</th>
+         <th>Badge</th>
+        </tr>
+        </thead>
+        <tbody>';
+
+
+foreach ($resultArr as $array) {
+  echo '<tr>
+            <td width="10%><img src="' . $array['photo'] . '" width="50px"/></td>
+            <td width="20%">Name - <b>' . $array['fullname'] . '</b><br>Associate ID - <b>' . $array['associatenumber'] . '</b>
+            <br><b>' . $array['gender'] . '&nbsp;(' . $array['age'] . ')<br><br>DOJ - ' . $array['doj'] . '</td>
+            <td width="15%">' . $array['position'] . '</td>
+            <td width="15%">' . $array['class'] . '</td>
+            <td width="15%"><span class="noticet"><a href="' . $array['gm'] . '" target="_blank">' . $array['gm'] . '</td>
+            <td>' . $array['astatus'] . '<br><br>' . $array['effectivedate'] . '&nbsp;' . $array['remarks'] . '</td>
+            <td>' . $array['today'] . '</td>
+            <td>' . $array['colors'] . '</td>
+            <td>' . $array['classtaken'] . '/' . $array['maxclass'] . '&nbsp' . $array['ctp'] . '</td>
+            <td>' . $array['badge'] . '</td>
+          </tr>';
+}
+echo '</table>';
+?>
---
+++ rssi-member/index_admin.php
@@ -0,0 +1,96 @@
+<?php
+session_start(); //session starts here  
+
+?>
+
+<html>
+
+<head lang="en">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon" />
+    <title>Admin</title>
+</head>
+<style>
+    .login-panel {
+        margin-top: 150px;
+    }
+
+    .x-btn:focus,
+    .button:focus,
+    [type="submit"]:focus {
+        outline: none;
+    }
+</style>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-success">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Sign In</h3>
+                    </div>
+                    <div class="panel-body">
+                        <form role="form" method="post" action="index_admin.php">
+                            <fieldset>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Associate ID" name="aid_admin" type="text" autofocus required>
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="Password" name="pass" type="password" value="" required>
+                                </div>
+                                <input class="btn btn-lg btn-success btn-block" type="submit" value="Login" name="login">
+
+                                <!-- Change this to a button or input when using this as a form -->
+                                <!--  <a href="index.html" class="btn btn-lg btn-success btn-block">Login</a> -->
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        if (window.history.replaceState) {
+            window.history.replaceState(null, null, window.location.href);
+        }
+    </script>
+</body>
+
+</html>
+
+<?php
+
+include("database.php");
+
+if (isset($_POST['login'])) {
+    $AssociateNumber = strtoupper($_POST['aid_admin']);
+    $colors = $_POST['pass'];
+
+    $check_user = "select * from memberdata WHERE AssociateNumber='VTHN20008'AND colors='$colors'";
+
+    $run = pg_query($con, $check_user);
+
+    if (pg_num_rows($run)) {
+        echo "<script>window.open('faculty.php','_self')</script>";
+
+        $_SESSION['aid_admin'] = 'VTHN20008'; //here session is used and value of $user_email store in $_SESSION. 
+
+    } else { ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4" style="text-align: center;">
+                    <span style="color:red">Error: Login failed. Please enter valid credentials.</span>
+                </div>
+            </div>
+        </div>
+
+
+<?php }
+}
+?>
