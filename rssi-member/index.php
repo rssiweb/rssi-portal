154e4e38d273c58fef8d776749ba22b2147f45a7
Somnath <sahasomnath93@gmail.com>
2021-06-09 23:42:08 +0530

    ok

--- rssi-member/index.php
+++ rssi-member/index.php
@@ -4,17 +4,18 @@ session_start(); //session starts here
 define('SITE_KEY', '6LfJRc0aAAAAAEhNPCD7ju6si7J4qRUCBSN_8RsL');
 define('SECRET_KEY', '6LfJRc0aAAAAAFuZLLd3_7KFmxQ7KPCZmLIiYLDH');
 
-if($_POST){
-    function getCaptcha($SecretKey){
-        $Response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".SECRET_KEY."&response={$SecretKey}");
+if ($_POST) {
+    function getCaptcha($SecretKey)
+    {
+        $Response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=" . SECRET_KEY . "&response={$SecretKey}");
         $Return = json_decode($Response);
         return $Return;
     }
     $Return = getCaptcha($_POST['g-recaptcha-response']);
     //var_dump($Return);
-    if($Return->success == true && $Return->score > 0.5){
+    if ($Return->success == true && $Return->score > 0.5) {
         echo "Succes!";
-    }else{
+    } else {
         echo "You are a Robot!!";
     }
 }
@@ -38,9 +39,12 @@ if($_POST){
     .login-panel {
         margin-top: 150px;
     }
-    .x-btn:focus, .button:focus, [type="submit"]:focus {
-   outline: none;
-}
+
+    .x-btn:focus,
+    .button:focus,
+    [type="submit"]:focus {
+        outline: none;
+    }
 </style>
 
 <body>
@@ -60,7 +64,7 @@ if($_POST){
                                 <div class="form-group">
                                     <input class="form-control" placeholder="Password" name="pass" type="password" value="" required>
                                 </div>
-                                <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response"/>
+                                <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response" />
                                 <input class="btn btn-lg btn-success btn-block" type="submit" value="Login" name="login">
 
                                 <!-- Change this to a button or input when using this as a form -->
@@ -94,32 +98,34 @@ if (isset($_POST['login'])) {
     $run = pg_query($con, $check_user);
 
     if (pg_num_rows($run)) {
-       echo "<script>window.open('home.php','_self')</script>";
+        echo "<script>window.open('home.php','_self')</script>";
 
         $_SESSION['aid'] = $associatenumber; //here session is used and value of $user_email store in $_SESSION.
 
         $row = pg_fetch_row($run);
-        $role= $row[62];
-        $filterstatus= $row[31];
+        $role = $row[62];
+        $filterstatus = $row[31];
 
         $_SESSION['role'] = $role;
-       $_SESSION['filterstatus'] = $filterstatus;
+        $_SESSION['filterstatus'] = $filterstatus;
 
-       //echo "<script>alert('";  
-       //echo $role;
-       //echo $filterstatus;
-       //echo "')</script>";
+        //echo "<script>alert('";  
+        //echo $role;
+        //echo $filterstatus;
+        //echo "')</script>";
 
 
-    }else {?>
+    } else { ?>
         <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4" style="text-align: center;">
-            <span style="color:red">Error: Login failed. Please enter valid credentials.</span>
-            </div></div></div>
-        
-        
-        <?php }
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4" style="text-align: center;">
+                    <span style="color:red">Error: Login failed. Please enter valid credentials.</span>
+                </div>
+            </div>
+        </div>
+
+
+<?php }
 }
 ?>
 
@@ -127,10 +133,12 @@ if (isset($_POST['login'])) {
 <!--protected by reCAPTCHA-->
 <script>
     grecaptcha.ready(function() {
-    grecaptcha.execute('<?php echo SITE_KEY; ?>', {action: 'homepage'})
-    .then(function(token) {
-        //console.log(token);
-        document.getElementById('g-recaptcha-response').value=token;
+        grecaptcha.execute('<?php echo SITE_KEY; ?>', {
+                action: 'homepage'
+            })
+            .then(function(token) {
+                //console.log(token);
+                document.getElementById('g-recaptcha-response').value = token;
+            });
     });
-    });
-    </script>
+</script>
