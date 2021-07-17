38172e96c048ee843095ca375a90adafc155ddfb
Somnath <sahasomnath93@gmail.com>
2021-07-17 20:03:01 +0530

    ok

--- rssi-student/question.php
+++ rssi-student/question.php
@@ -8,12 +8,12 @@ if (!$_SESSION['sid']) {
     $_SESSION["login_redirect"] = $_SERVER["PHP_SELF"];
     header("Location: index.php");
     exit;  
-} else if ($_SESSION['filterstatus'] != 'Active') {
+} else if (($_SESSION['filterstatus'] != 'Active') || ($_SESSION['feesflag'] == 'd')){
 
     //header("Location: javascript:history.back()"); //redirect to the login page to secure the welcome page without login access.
     echo '<script type="text/javascript">';
     echo 'alert("Access Denied. You are not authorized to access this web page.");';
-    echo 'window.location.href = "home.php";';
+    echo 'window.location.href = "profile.php";';
     echo '</script>';
 }
 ?>
