d51bcefcfc672baa26796e642459b54bba836281
Somnath Saha <sahasomnath93@gmail.com>
2021-12-07 22:22:34 +0530

    ok

--- rssi-member/student.php
+++ rssi-member/student.php
@@ -262,7 +262,7 @@ $resultArr = pg_fetch_all($result);
           } else {
           ?>
             <tr>
-              <td colspan="5">No record found for <?php echo $module ?>, <?php echo $id ?> and <?php echo $category ?></td>
+              <td colspan="5">No record found for <?php echo $module ?>, <?php echo $id ?> and <?php echo $category ?>&nbsp;<?php echo $class ?></td>
             </tr>
           <?php }
 
