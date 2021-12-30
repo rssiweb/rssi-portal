5e22ccc74e45b9d9c4b1b528aa71ebb912306685
Somnath Saha <sahasomnath93@gmail.com>
2021-12-31 01:26:10 +0530

    ok

--- rssi-member/home.php
+++ rssi-member/home.php
@@ -280,7 +280,7 @@ while ($row = pg_fetch_array($run)) //while look to fetch the result and store i
 
     <!--**************QUESTION PAPER SUBMISSION CONFIRMATION**************-->
     <?php
-    if ((@$googlechat == null) && $filterstatus == 'Active' && @$ipfl != null) {
+    if ((@$googlechat == null) && $filterstatus == 'Active' && @$remarks != 'Reg') {
     ?>
 
         <div id="thoverX" class="thover pop-up2"></div>
@@ -290,23 +290,26 @@ while ($row = pg_fetch_array($run)) //while look to fetch the result and store i
                 <input type="hidden" class="form-control" name="membername2" type="text" value="<?php echo $fullname ?>" readonly>
                 <input type="hidden" class="form-control" name="memberid2" type="text" value="<?php echo $associatenumber ?>" readonly>
                 <input type="hidden" type="text" name="status2" id="count2" value="" readonly required>
-                <p style="white-space:normal !important;word-wrap:break-word;">Hi&nbsp;<?php echo strtok($fullname, ' ') ?>, Your IPF / Monthly Feedback has been released. For more details, please visit
-                    <span class="noticet"><a href="my_appraisal.php" target="_blank">My Appraisal</a> portal.</span>
+                <p style="white-space:normal !important;word-wrap:break-word;">Hi&nbsp;<?php echo strtok($fullname, ' ') ?>, Your offer letter has been revised and the offer has been extended for the academic year 2022-2023. You can access your offer letter anytime from the My Document section.
+                    <!--<span class="noticet"><a href="my_appraisal.php" target="_blank">My Appraisal</a> portal.</span>-->
                 </p>
-               <br>
-                <button type="submit" id="yes" class="close-button2 btn btn-success" style="width: 20%; white-space:normal !important;word-wrap:break-word;">I have checked my IPF and I accept it.</button>
-                <button type="submit" id="no" class="close-button2 btn btn-danger" style="width: 20%; white-space:normal !important;word-wrap:break-word;">I have checked my IPF and I reject it.
-                </button>
+                <embed class="hidden-xs" src="https://drive.google.com/file/d/<?php echo $questionflag ?>/preview" width="700px" height="400px" /></embed>
+                <span class="noticet hidden-md hidden-sm hidden-lg"><a href="<?php echo $profile ?>" target="_blank"><?php echo $filename ?></a></span>
+                <br><br>
+
+                <button type="submit" id="yes" class="btn btn-success btn-sm close-button2" style="white-space:normal !important;word-wrap:break-word;" onclick="location.href='https://docs.google.com/forms/d/e/1FAIpQLSdBTkdA-jqrPqZBLebQV3vlBiVk2iklcbZkn1Z1pbZIQISb3g/formResponse?usp=pp_url&entry.1000022=<?php echo $associatenumber ?>&entry.1000020=<?php echo $fullname ?>&entry.1000025=<?php echo $email ?>&entry.1701149099=Accepted';">I accept the offer</button>
+                <button type="submit" id="no" class="btn btn-danger btn-sm close-button2" style="white-space:normal !important;word-wrap:break-word;" onclick="location.href='https://docs.google.com/forms/d/e/1FAIpQLSdBTkdA-jqrPqZBLebQV3vlBiVk2iklcbZkn1Z1pbZIQISb3g/formResponse?usp=pp_url&entry.1000022=<?php echo $associatenumber ?>&entry.1000020=<?php echo $fullname ?>&entry.1000025=<?php echo $email ?>&entry.1701149099=Rejected';">I reject the offer</button>
+
                 <br><br>
             </form>
         </div>
         <script>
             $('#yes').click(function() {
-                $('#count2').val('I have checked my IPF and I accept it.');
+                $('#count2').val('I accept the offer');
             });
 
             $('#no').click(function() {
-                $('#count2').val('I have checked my IPF and I reject it.');
+                $('#count2').val('I reject the offer');
             });
         </script>
         <script>
@@ -331,7 +334,7 @@ while ($row = pg_fetch_array($run)) //while look to fetch the result and store i
         <script>
             $(document).ready(function() {
 
-                if (Boolean(readCookie('Appraisal'))) {
+                if (Boolean(readCookie('Offer'))) {
                     $('.pop-up2').hide();
                     $('.pop-up2').fadeOut(1000);
                 }
@@ -340,7 +343,7 @@ while ($row = pg_fetch_array($run)) //while look to fetch the result and store i
                     $('.pop-up2').delay(10).fadeOut(700);
                     e.stopPropagation();
 
-                    createCookie("Appraisal", "4 days", 4);
+                    createCookie("Offer", "15 days", 15);
                     //return false;
                 });
 
