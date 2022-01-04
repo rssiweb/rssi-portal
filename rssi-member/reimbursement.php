1e58f4f42f5c68ae7436d77826d352aca18902b3
Somnath Saha <sahasomnath93@gmail.com>
2022-01-04 19:58:48 +0530

    ok

---
+++ rssi-member/reimbursement.php
@@ -0,0 +1,149 @@
+<?php
+session_start();
+// Storing Session
+$user_check = $_SESSION['aid'];
+
+if (!$_SESSION['aid']) {
+
+    $_SESSION["login_redirect"] = $_SERVER["PHP_SELF"];
+    header("Location: index.php");
+    exit;  
+}
+?>
+
+<?php
+include("member_data.php");
+include("database.php");
+$view_users_query = "select * from claim WHERE registrationid='$user_check'"; //select query for viewing users.  
+$run = pg_query($con, $view_users_query); //here run the sql query.  
+
+while ($row = pg_fetch_array($run)) //while look to fetch the result and store in a array $row.  
+{
+$id=$row[0];
+$timestamp=$row[1];
+$name=$row[2];
+$registrationid=$row[3];
+$mobilenumber=$row[4];
+$email=$row[5];
+$bankname=$row[6];
+$accountnumber=$row[7];
+$accountholdername=$row[8];
+$ifsccode=$row[9];
+$selectclaimheadfromthelistbelow=$row[10];
+$billno=$row[11];
+$currency=$row[12];
+$totalbillamount=$row[13];
+$uploadeddocuments=$row[14];
+$ack=$row[15];
+$termsofagreement=$row[16];
+$year=$row[17];
+$reimbid=$row[18];
+$mergestatus=$row[19];
+$claimstatus=$row[20];
+$approvedamount=$row[21];
+$transactionid=$row[22];
+$transfereddate=$row[23];
+$closedon=$row[24];
+$mediremarks=$row[25];
+$profile=$row[26];
+$rlastupdatedon=$row[27]      
+
+?>
+<?php } ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>Reimbursement</title>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon" />
+    <!-- Main css -->
+    <style>
+        <?php include '../css/style.css'; ?>
+    </style>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
+
+    <script src="https://cdn.jsdelivr.net/gh/manucaralmo/GlowCookies@3.0.1/src/glowCookies.min.js"></script>
+    <!-- Glow Cookies v3.0.1 -->
+    <script>
+        glowCookies.start('en', {
+            analytics: 'G-S25QWTFJ2S',
+            //facebookPixel: '',
+            policyLink: 'https://drive.google.com/file/d/1o-ULIIYDLv5ipSRfUa6ROzxJZyoEZhDF/view'
+        });
+    </script>
+
+</head>
+
+<body>
+    <?php $medimate_active = 'active'; ?>
+    <?php include 'header.php'; ?>
+
+    <section id="main-content">
+        <section class="wrapper main-wrapper row">
+            <div class="col-md-12">
+                <div class=col style="text-align: right;"><a href="medistatus.php"><button type="button" class="exam_btn"><i class="fas fa-child" style="font-size: 17px;"></i>
+                Track Your Claim</button></a>
+                </div>
+                <section class="box" style="padding: 2%;">
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Reimbursement policy</th>
+                                <th scope="col">Submit claim</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td style="line-height: 2;"><span class="noticet"><a href="javascript:void(0)" target="_self">RSSI reimbursement policy</a></span></td>
+                                <td style="line-height: 2;"><span class="noticet"><a href="https://docs.google.com/forms/d/e/1FAIpQLSeJvl1DPzvAHpEfGLdPhnjK1ojTcRNnLR_w1WCCswDxJTZxlg/viewform?usp=pp_url&entry.1268051974=<?php echo @$fullname ?>&entry.288127209=<?php echo @$associatenumber ?>&entry.995125243=7980168159&entry.605633398=<?php echo @$email ?>&entry.1867197840=<?php echo @$bankname ?>&entry.1288695359=<?php echo @$accountnumber ?>&entry.1236133419=<?php echo @$accountholdername ?>&entry.1547933107=<?php echo @$ifsccode ?>" target="_blank">Claim form</a></span></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Bank Account Details</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td style="line-height: 2; font-size:13px">
+                                    <?php echo @$bankname ?><br>
+                                    Account Number:&nbsp;<b><?php echo @$accountnumber ?></b><br>
+                                    Account Holder Name:&nbsp;<?php echo @$accountholdername ?><br>
+                                    IFSC Code:&nbsp;<?php echo @$ifsccode ?>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
+                <div style="font-size: 13px;">
+                <p><b>Note:</b></p>
+                    Once the claim is settled, amount will be credited to applicant's account within 5 to 6 working days from date of Settlement.</p>
+                    <p>For the first time you have to fill the complete Domiciliary Claim Form. From the second time onwards your bank account details will be automatically updated based on your previous information.</p>
+                    <br>Last updated on: <?php echo @$rlastupdatedon ?>
+                    </div>
+            </div>
+
+            <div class="clearfix"></div>
+            <!--**************clearfix**************
+
+           <div class="col-md-12">
+                <section class="box">cccccccccccee33</section>
+            </div>-->
+
+        </section>
+    </section>
+</body>
+
+</html>
---
+++ rssi-member/reimbursementstatus.php
@@ -0,0 +1,231 @@
+<?php
+session_start();
+// Storing Session
+$user_check = $_SESSION['aid'];
+
+if (!$_SESSION['aid']) {
+
+    $_SESSION["login_redirect"] = $_SERVER["PHP_SELF"];
+    header("Location: index.php");
+    exit;
+}
+?>
+
+<?php
+include("member_data.php");
+?>
+<?php
+include("database.php");
+@$id = $_POST['get_id'];
+$result = pg_query($con, "SELECT * FROM claim WHERE registrationid='$user_check' AND year='$id' order by id desc");
+if (!$result) {
+    echo "An error occurred.\n";
+    exit;
+}
+
+$resultArr = pg_fetch_all($result);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>Reimbursement Status</title>
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon" />
+    <!-- Main css -->
+    <!-- Main css -->
+    <style>
+        <?php include '../css/style.css'; ?>
+    </style>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
+
+    <script src="https://cdn.jsdelivr.net/gh/manucaralmo/GlowCookies@3.0.1/src/glowCookies.min.js"></script>
+    <!-- Glow Cookies v3.0.1 -->
+    <script>
+        glowCookies.start('en', {
+            analytics: 'G-S25QWTFJ2S',
+            //facebookPixel: '',
+            policyLink: 'https://drive.google.com/file/d/1o-ULIIYDLv5ipSRfUa6ROzxJZyoEZhDF/view'
+        });
+    </script>
+    <style>
+        @media (max-width:767px) {
+
+            #cw,
+            #cw1,
+            #cw2,
+            #cw3 {
+                width: 100% !important;
+            }
+
+        }
+
+        #cw {
+            width: 15%;
+        }
+
+        #cw1 {
+            width: 20%;
+        }
+
+        #cw2 {
+            width: 25%;
+        }
+
+        #cw3 {
+            width: 20%;
+        }
+
+        @media (min-width:767px) {
+            .left {
+                margin-left: 2%;
+            }
+        }
+    </style>
+</head>
+
+<!-- =========================
+     NAVIGATION LINKS     
+============================== -->
+
+<body>
+    <?php include 'header.php'; ?>
+    <section id="main-content">
+        <section class="wrapper main-wrapper row">
+            <div class="col-md-12">
+                <div class=col style="text-align: right;">
+                    <!--<span class="noticet" style="line-height: 2;"><a href="#" onClick="javascript:history.go(-1)">Back to previous page</a></span><br>-->
+                    Policy year: <?php echo $id ?>
+                </div>
+                <section class="box" style="padding: 2%;">
+                    <form action="" method="POST">
+                        <div class="form-group" style="display: inline-block;">
+                            <div class="col2" style="display: inline-block;">
+                                <select name="get_id" class="form-control" style="width:max-content;" placeholder="Select policy year" required>
+                                    <?php if ($id == null) { ?>
+                                        <option value="" hidden selected>Select policy year</option>
+                                    <?php
+                                    } else { ?>
+                                        <option hidden selected><?php echo $id ?></option>
+                                    <?php }
+                                    ?>
+                                    <option>2022</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col2 left" style="display: inline-block;">
+                            <button type="submit" name="search_by_id" class="btn btn-primary" style="outline: none;">
+                                <span class="glyphicon glyphicon-search"></span>&nbsp;Search</button>
+                        </div>
+                    </form>
+                    <?php echo '
+                    <table class="table">
+                        <thead style="font-size: 12px;">
+                        <tr>    
+                        <th scope="col">Claim Number</th>
+                        <th scope="col">Registered On</th>
+                        <th scope="col">Bills</th>
+                        <th scope="col">Account Number</th>
+                        <th scope="col">Claimed Amount (&#8377;)</th>
+                        <th scope="col">Amount Transfered (&#8377;)</th>
+                        <th scope="col">Transaction Reference Number</th>
+                        <th scope="col">Transfered Date</th>
+                        <th scope="col">Claim Status</th>
+                        <th scope="col">Closed on</th>
+                        <th scope="col">Remarks</th>
+                    </tr>
+                        </thead>' ?>
+                    <?php if ($resultArr != null) {
+                        echo '<tbody style="font-size: 13px;">';
+                        foreach ($resultArr as $array) {
+                            echo '
+                            <tr>
+                                    <td>' . $array['reimbid'] . '</td>
+                                    <td>' . substr($array['timestamp'], 0, 10) . '</td>
+                                    <td><span><a href="' . $array['uploadeddocuments'] . '" target="_blank"><i class="far fa-file-pdf" style="font-size:17px;color: #767676;"></i></a></span></td>
+                                    <td>' . $array['accountnumber']. '</td>
+                                    <td>' . $array['totalbillamount'] . '</td>
+                                    <td>' . $array['approvedamount'] . '</td>
+                                    <td>' . $array['transactionid'] . '</td>
+                                    <td>' . $array['transfereddate'] . '</td>'
+                    ?>
+                            <?php if ($array['claimstatus'] == 'review' || $array['claimstatus'] == 'in progress' || $array['claimstatus'] == 'withdrawn') { ?>
+                                <?php echo '<td> <p class="label label-warning">' . $array['claimstatus'] . '</p>' ?>
+
+                            <?php } else if ($array['claimstatus'] == 'approved' || $array['claimstatus'] == 'claim settled') { ?>
+                                <?php echo '<td><p class="label label-success">' . $array['claimstatus'] . '</p>' ?>
+                            <?php    } else if ($array['claimstatus'] == 'rejected' || $array['claimstatus'] == 'on hold') { ?>
+                                <?php echo '<td><p class="label label-danger">' . $array['claimstatus'] . '</p>' ?>
+                            <?php    } else { ?>
+                                <?php echo '<td><p class="label label-info">' . $array['claimstatus'] . '</p>' ?>
+                            <?php } ?>
+
+
+                            <?php echo
+                            '</td><td>' . $array['closedon'] . '</td>
+                                    <td>' . $array['mediremarks'] . '</td>
+                                    </tr>';
+                        }
+                    } else if ($id == null) {
+                        echo '<tr>
+                            <td  colspan="2">Please select policy year.</td>
+                        </tr>';
+                    } else {
+                        echo '<tr>
+                        <td  colspan="2">No record found for' ?>&nbsp;<?php echo $id ?>
+                        <?php echo '</td>
+                    </tr>';
+                    }
+                    echo '</tbody>
+                     </table>';
+                        ?>
+            </div>
+            <div class="col-md-12" style="text-align: right;">
+                <span class="noticet" style="line-height: 2;"><a href="medimate.php">Back to Medimate</a></span>
+            </div>
+            </div>
+        </section>
+        </div>
+    </section>
+    </section>
+
+
+    <!-- Back top -->
+    <script>
+        $(document).ready(function() {
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > 50) {
+                    $('#back-to-top').fadeIn();
+                } else {
+                    $('#back-to-top').fadeOut();
+                }
+            });
+            // scroll body to 0px on click
+            $('#back-to-top').click(function() {
+                $('body,html').animate({
+                    scrollTop: 0
+                }, 400);
+                return false;
+            });
+        });
+    </script>
+    <!-- TEXT CLICK POPUP CSS and SCRIPT-->
+    <!--<script>
+        // When the user clicks on div, open the popup
+        function myFunction() {
+            var popup = document.getElementById("myPopup");
+            popup.classList.toggle("show");
+        }
+    </script>-->
+    <a id="back-to-top" href="#" class="go-top" role="button"><i class="fa fa-angle-up"></i></a>
+</body>
+
+</html>
