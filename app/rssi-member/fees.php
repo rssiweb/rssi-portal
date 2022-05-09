948fab5ec67c47eb0d03149c0fdfadbc3d806d71
Somnath Saha <sahasomnath93@gmail.com>
2022-05-09 18:20:55 +0530

    ok

---
+++ app/rssi-member/fees.php
@@ -0,0 +1,206 @@
+<?php
+session_start();
+// Storing Session
+include("../util/login_util.php");
+
+if (!isLoggedIn("aid")) {
+  header("Location: index.php");
+}
+$user_check = $_SESSION['aid'];
+
+if (!$_SESSION['aid']) {
+
+  $_SESSION["login_redirect"] = $_SERVER["PHP_SELF"];
+  header("Location: index.php");
+  exit;
+} else if ($_SESSION['role'] != 'Admin') {
+
+  //header("Location: javascript:history.back()"); //redirect to the login page to secure the welcome page without login access.
+  echo '<script type="text/javascript">';
+  echo 'alert("Access Denied. You are not authorized to access this web page.");';
+  echo 'window.location.href = "home.php";';
+  echo '</script>';
+}
+?>
+<?php
+include("member_data.php");
+?>
+<?php
+include("database.php");
+@$id = $_POST['get_aid'];
+@$status = $_POST['get_id'];
+
+  $result = pg_query($con, "SELECT * FROM fees WHERE month=$status AND DATE_PART('year', date::date)=$id order by id desc");
+  $totalapprovedamount = pg_query($con, "SELECT SUM(fees) FROM fees WHERE month=$status AND DATE_PART('year', date::date)=$id");
+
+if (!$result) {
+  echo "An error occurred.\n";
+  exit;
+}
+
+$resultArr = pg_fetch_all($result);
+$resultArrr = pg_fetch_result($totalapprovedamount, 0, 0);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <title>Reimbursement Admin</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon" />
+  <!-- Main css -->
+  <style>
+    <?php include '../css/style.css'; ?>
+  </style>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+  <script src="https://kit.fontawesome.com/58c4cdb942.js" crossorigin="anonymous"></script>
+  <!------ Include the above in your HEAD tag ---------->
+
+  <script src="https://cdn.jsdelivr.net/gh/manucaralmo/GlowCookies@3.0.1/src/glowCookies.min.js"></script>
+  <!-- Glow Cookies v3.0.1 -->
+  <script>
+    glowCookies.start('en', {
+      analytics: 'G-S25QWTFJ2S',
+      //facebookPixel: '',
+      policyLink: 'https://drive.google.com/file/d/1o-ULIIYDLv5ipSRfUa6ROzxJZyoEZhDF/view'
+    });
+  </script>
+  <style>
+    @media (min-width:767px) {
+      .left {
+        margin-left: 2%;
+      }
+    }
+
+    #btn {
+      border: none !important;
+    }
+  </style>
+
+</head>
+
+<!-- =========================
+     NAVIGATION LINKS     
+============================== -->
+
+<body>
+  <?php include 'header.php'; ?>
+  <section id="main-content">
+    <section class="wrapper main-wrapper row">
+      <div class="col-md-12">
+        <div class="row">
+          <div class="col" style="display: inline-block; width:50%;margin-left:1.5%; font-size:small">
+            Record count:&nbsp;<?php echo sizeof($resultArr) ?><br>Total Approved amount:&nbsp;<p class="label label-default"><?php echo ($resultArrr) ?></p>
+          </div>
+          <!-- <div class="col" style="display: inline-block; width:47%; text-align:right; font-size:small">
+            Home / Reimbursement Status
+          </div> -->
+        </div>
+        <section class="box" style="padding: 2%;">
+          <form action="" method="POST">
+            <div class="form-group" style="display: inline-block;">
+              <div class="col2" style="display: inline-block;">
+
+              <select name="get_aid" class="form-control" style="width:max-content; display:inline-block" placeholder="Select policy year" required>
+                  <?php if ($id == null) { ?>
+                    <option value="" hidden selected>Select policy year</option>
+                  <?php
+                  } else { ?>
+                    <option hidden selected><?php echo $id ?></option>
+                  <?php }
+                  ?>
+                  <option>2022</option>
+                  <option>2021</option>
+                </select>
+
+                <select name="get_id" class="form-control" style="width:max-content; display:inline-block" placeholder="Select policy year" required>
+                  <?php if ($status == null) { ?>
+                    <option value="" hidden selected>Select policy year</option>
+                  <?php
+                  } else { ?>
+                    <option hidden selected><?php echo $status ?></option>
+                  <?php }
+                  ?>
+                  <option>4</option>
+                </select>
+              </div>
+            </div>
+            <div class="col2 left" style="display: inline-block;">
+              <button type="submit" name="search_by_id" class="btn btn-success btn-sm" style="outline: none;">
+                <i class="fa-solid fa-magnifying-glass"></i>&nbsp;Search</button>
+            </div>
+          </form>
+          <?php echo '
+                        <table class="table">
+                            <thead style="font-size: 12px;">
+                                <tr>
+                                <th scope="col">Claim Number</th>
+                                <th scope="col">Registered On</th>    
+                                <th scope="col">ID/F Name</th>
+                                <th scope="col">Purpose</th>
+                                <th scope="col">Claimed Amount (&#8377;)</th>
+                                </tr>
+                            </thead>' ?>
+          <?php if ($resultArr != null) {
+            echo '<tbody>';
+            foreach ($resultArr as $array) {
+              echo '<tr><td>' . substr($array['date'], 0, 10) . '</td>
+                        <td>' . $array['studentid'] . '/' . strtok($array['sname'], ' ') . '</td>   
+                        <td>' . $array['fees'] . '</td>
+                        <td>' . $array['collectedby'] . '</td>' ?>
+                        <?php  }
+          } else if ($status == null) {
+            echo '<tr>
+                                <td colspan="5">Please select policy year.</td>
+                            </tr>';
+          } else {
+            echo '<tr>
+                            <td colspan="5">No record found for' ?>&nbsp;<?php echo $status ?>
+            <?php echo '</td>
+                        </tr>';
+          }
+          echo '</tbody>
+                        </table>';
+            ?>
+        </section>
+      </div>
+    </section>
+  </section>
+
+  <!-- Back top -->
+  <script>
+    $(document).ready(function() {
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) {
+          $('#back-to-top').fadeIn();
+        } else {
+          $('#back-to-top').fadeOut();
+        }
+      });
+      // scroll body to 0px on click
+      $('#back-to-top').click(function() {
+        $('body,html').animate({
+          scrollTop: 0
+        }, 400);
+        return false;
+      });
+    });
+  </script>
+  <a id="back-to-top" href="#" class="go-top" role="button"><i class="fa fa-angle-up"></i></a>
+
+
+ 
+
+
+</body>
+
+</html>
