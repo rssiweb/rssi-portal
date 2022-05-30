6572637f9c761348305bfd944784e2fcbd34f03a
Somnath Saha <sahasomnath93@gmail.com>
2022-05-30 11:18:41 +0530

    ok

--- app/rssi-member/student-profile.php
+++ app/rssi-member/student-profile.php
@@ -25,68 +25,15 @@ $date = date('Y-m-d H:i:s');
 
 <?php
 include("database.php");
-$view_users_query = "select * from rssimyprofile_student WHERE student_id='$id'"; //select query for viewing users.  
-$run = pg_query($con, $view_users_query); //here run the sql query.  
-$resultArr = pg_fetch_all($run);
+$result = pg_query($con, "select * from rssimyprofile_student WHERE student_id='$id'"); //select query for viewing users.    
+$resultArr = pg_fetch_all($result);
 
-while ($row = pg_fetch_array($run)) //while look to fetch the result and store in a array $row.  
-{
-    $category = $row[0];
-    $student_id = $row[1];
-    $roll_number = $row[2];
-    $studentname = $row[3];
-    $gender = $row[4];
-    $age = $row[5];
-    $class = $row[6];
-    $profile = $row[7];
-    $contact = $row[8];
-    $guardiansname = $row[9];
-    $relationwithstudent = $row[10];
-    $studentaadhar = $row[11];
-    $guardianaadhar = $row[12];
-    $dateofbirth = $row[13];
-    $postaladdress = $row[14];
-    $nameofthesubjects = $row[15];
-    $preferredbranch = $row[16];
-    $nameoftheschool = $row[17];
-    $nameoftheboard = $row[18];
-    $stateofdomicile = $row[19];
-    $emailaddress = $row[20];
-    $schooladmissionrequired = $row[21];
-    $status = $row[22];
-    $remarks = $row[23];
-    $nameofvendorfoundation = $row[24];
-    $photourl = $row[25];
-    $familymonthlyincome = $row[26];
-    $totalnumberoffamilymembers = $row[27];
-    $medium = $row[28];
-    $fees = $row[29];
-    $bookststus = $row[30];
-    $mydocument = $row[31];
-    $profilestatus = $row[32];
-    $lastupdatedon = $row[33];
-    $colors = $row[34];
-    $classurl = $row[35];
-    $remarks1 = $row[36];
-    $notice = $row[37];
-    $badge = $row[38];
-    $filterstatus = $row[39];
-    $allocationdate = $row[40];
-    $maxclass = $row[41];
-    $attd = $row[42];
-    $leaveapply = $row[43];
-    $cltaken = $row[44];
-    $sltaken = $row[45];
-    $othtaken = $row[46];
-    $filename = $row[47];
-    $lastlogin = $row[48];
-    $doa = $row[49];
-    $feesflag = $row[50];
-    $module = $row[51];
-    $upload_aadhar_card = $row[58];
+if (!$result) {
+    echo "An error occurred.\n";
+    exit;
+}
 
 ?>
-<?php } ?>
 
 <!DOCTYPE html>
 <html>
@@ -97,14 +44,15 @@ while ($row = pg_fetch_array($run)) //while look to fetch the result and store i
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <title><?php echo $studentname ?>_<?php echo $student_id ?></title>
+    <title><?php echo $id ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon" />
     <!-- Main css -->
     <style>
-        <?php include '../css/style.css'; ?>
-        @media print {
-        @page {size: A4 landscape; }
+        <?php include '../css/style.css'; ?>@media print {
+            @page {
+                size: A4 landscape;
+            }
         }
     </style>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
@@ -144,15 +92,17 @@ while ($row = pg_fetch_array($run)) //while look to fetch the result and store i
             </form>
             <?php if ($resultArr != null) { ?>
                 <div class="col" style="display: inline-block; width:55%; text-align:left">
-                    
+
                     <p><b>Rina Shiksha Sahayak Foundation (RSSI)</b></p>
                     <p>1074/801, Jhapetapur, Backside of Municipality, West Midnapore, West Bengal 721301</p>
                 </div>
                 <div class="col" style="display: inline-block; width:42%;margin-left:1.5%;text-align:right;">
-                    <img class="qrimage" src="https://chart.googleapis.com/chart?chs=85x85&cht=qr&chl=https://login.rssi.in/rssi-student/verification.php?get_id=<?php echo $student_id ?>" width="74px" />
+                    <img class="qrimage" src="https://chart.googleapis.com/chart?chs=85x85&cht=qr&chl=https://login.rssi.in/rssi-student/verification.php?get_id=<?php echo $id ?>" width="74px" />
                 </div>
 
-                <table class="table">
+                <?php foreach ($resultArr as $array) {
+
+                    echo '<table class="table">
                     <thead style="font-size: 12px;">
                         <tr>
                             <th scope="col">Photo</th>
@@ -162,13 +112,13 @@ while ($row = pg_fetch_array($run)) //while look to fetch the result and store i
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td><img src=<?php echo $photourl ?> width=75px /></td>
-                            <td style="line-height: 1.7;"><b><?php echo $studentname ?></b><br>Student ID - <b><?php echo $student_id ?></b>, Roll No - <b><?php echo $roll_number ?></b><br>
-                                <span style="line-height: 3;"><?php echo $gender ?> (<?php echo $age ?> Years)</span>
+                    <tr>
+                            <td><img src= ' . $array['photourl'] . ' width=75px /></td>
+                            <td style="line-height: 1.7;"><b>' . $array['studentname'] . '</b><br>Student ID - <b>' . $array['student_id'] . '</b>, Roll No - <b>' . $array['roll_number'] . '</b><br>
+                                <span style="line-height: 3;">' . $array['gender'] . '(' . $array['age'] . 'Years)</span>
                             </td>
-                            <td><?php echo $filterstatus ?><br><br><?php echo $remarks1 ?></td>
-                            <td class="no-print"><?php echo @$badge ?></td>
+                            <td>' . $array['filterstatus'] . '<br><br>' . $array['remarks1'] . '</td>
+                            <td class="no-print">' . $array['badge'] . '</td>
                         </tr>
                     </tbody>
                 </table>
@@ -186,20 +136,25 @@ while ($row = pg_fetch_array($run)) //while look to fetch the result and store i
                     </thead>
                     <tbody>
                         <tr>
-                            <td><?php echo $doa ?></td>
-                            <td><?php echo $preferredbranch ?></td>
-                            <td><?php echo $class ?>/<?php echo $category ?></td>
-                            <td><?php echo $dateofbirth ?></td>
-                            <td><?php echo $studentaadhar ?></td>
-                            <td class="no-print"><iframe sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="https://drive.google.com/file/d/<?php echo substr($upload_aadhar_card, strpos($upload_aadhar_card, "=") + 1) ?>/preview" width="300px" height="200px" /></iframe></td>
-                        </tr>
-                    </tbody>
+                            <td>' . $array['doa'] . '</td>
+                            <td>' . $array['preferredbranch'] . '</td>
+                            <td>' . $array['class'] . '/' . $array['category'] . '</td>
+                            <td>' . $array['dateofbirth'] . '</td>
+                            <td>' . $array['studentaadhar'] . '</td>' ?>
+
+                    <?php if ($array['upload_aadhar_card'] != null) {
+
+                        echo '<td class="no-print"><iframe sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="https://drive.google.com/file/d/' . substr(@$array['upload_aadhar_card'], strpos(@$array['upload_aadhar_card'], "=") + 1) . '/preview" width="300px" height="200px" /></iframe></td>' ?>
+                        <?php  } else {
+                        echo '<td class="no-print">No document uploaded.</td>'
+                        ?><?php }
+                        echo '</tr></tbody>
                 </table>
 
                 <table class="table">
                     <thead style="font-size: 12px;">
                         <tr>
-                            <th scope="col">Guardian's Name</th>
+                            <th scope="col">Guardians Name</th>
                             <th scope="col">Guardian Aadhaar</th>
                             <th scope="col">Postal Address</th>
                             <th scope="col">Contact/Email Address</th>
@@ -209,12 +164,12 @@ while ($row = pg_fetch_array($run)) //while look to fetch the result and store i
                     </thead>
                     <tbody>
                         <tr>
-                            <td><?php echo $guardiansname ?> - <?php echo $relationwithstudent ?></td>
-                            <td><?php echo $guardianaadhar ?></td>
-                            <td><?php echo $postaladdress ?></td>
-                            <td style="line-height: 1.5;"><?php echo $contact ?><br><?php echo $emailaddress ?></td>
-                            <td><?php echo $familymonthlyincome ?></td>
-                            <td><?php echo $totalnumberoffamilymembers ?></td>
+                            <td>' . $array['guardiansname'] . ' - ' . $array['relationwithstudent'] . '</td>
+                            <td>' . $array['guardianaadhar'] . '</td>
+                            <td>' . $array['postaladdress'] . '</td>
+                            <td style="line-height: 1.5;">' . $array['contact'] . '<br>' . $array['emailaddress'] . '</td>
+                            <td>' . $array['familymonthlyincome'] . '</td>
+                            <td>' . $array['totalnumberoffamilymembers'] . '</td>
                         </tr>
                     </tbody>
                 </table>
@@ -230,19 +185,20 @@ while ($row = pg_fetch_array($run)) //while look to fetch the result and store i
                     </thead>
                     <tbody>
                         <tr>
-                            <td><?php echo $schooladmissionrequired ?></td>
-                            <td><?php echo $nameofthesubjects ?></td>
-                            <td><?php echo $nameoftheschool ?></td>
-                            <td><?php echo $nameoftheboard ?></td>
-                            <td><?php echo $medium ?></td>
+                            <td>' . $array['schooladmissionrequired'] . '</td>
+                            <td>' . $array['nameofthesubjects'] . '</td>
+                            <td>' . $array['nameoftheschool'] . '</td>
+                            <td>' . $array['nameoftheboard'] . '</td>
+                            <td>' . $array['medium'] . '</td>
                         </tr>
                     </tbody>
                 </table><br>
 
-                <p style="text-align:right;">Admission form generated: <?php echo $date ?></p>
+                <p style="text-align:right;">Admission form generated:' ?><?php echo $date ?><?php echo '</p>' ?>
 
-            <?php } else { ?>
-                <p class="no-print">Please enter student ID.</p> <?php } ?>
+                    <?php }
+            } else { ?>
+                    <p class="no-print">Please enter student ID.</p> <?php } ?>
         </section>
     </div>
 </body>
