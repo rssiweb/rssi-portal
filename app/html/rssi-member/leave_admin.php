86a8e4439b7a30efa3b55ee6357c80d40d31b953
Somnath Saha <sahasomnath93@gmail.com>
2023-01-31 10:05:31 +0530

    ok

--- app/html/rssi-member/leave_admin.php
+++ app/html/rssi-member/leave_admin.php
@@ -258,7 +258,7 @@ if (!$result) {
                                         <option value="" disabled selected hidden>Types of Leave</option>
                                         <option value="Sick Leave">Sick Leave</option>
                                         <option value="Casual Leave">Casual Leave</option>
-                                        <option value="Casual Leave">Leave Without Pay</option>
+                                        <option value="Leave Without Pay">Leave Without Pay</option>
                                     </select>
                                     <small id="passwordHelpBlock" class="form-text text-muted">Types of Leave</small>
                                 </span>
