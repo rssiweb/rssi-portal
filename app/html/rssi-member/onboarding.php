ee222dd568ae34b9bebaf55571c5ff03f3d0fda8
Somnath Saha <sahasomnath93@gmail.com>
2023-05-18 17:30:28 +0530

    ok

--- app/html/rssi-member/onboarding.php
+++ app/html/rssi-member/onboarding.php
@@ -64,6 +64,11 @@ if (@$auth_failed_dialog) { ?>
     <div class="alert alert-danger alert-dismissible" role="alert" style="text-align: -webkit-center;">
         <i class="fa-solid fa-xmark"></i>&nbsp;&nbsp;<span>ERROR: The OTP you entered is incorrect.</span>
     </div>
+    <script>
+        if (window.history.replaceState) {
+            window.history.replaceState(null, null, window.location.href);
+        }
+    </script>
 <?php } ?>
 <?php
 if (@$cmdtuples == 1) {
