fdc21eb4db33460fb8bcc894aefb3f9b5c5944fa
Somnath Saha <sahasomnath93@gmail.com>
2024-12-18 12:23:48 +0530

    ok

--- app/util/login_util_tap.php
+++ app/util/login_util_tap.php
@@ -11,9 +11,20 @@ function isLoggedIn(string $key)
 function passwordCheck($password_updated_by, $password_updated_on, $default_pass_updated_on)
 {
     if ($password_updated_by == null || $password_updated_on < $default_pass_updated_on) {
-        // Redirect to default password reset page
-        header("Location: defaultpasswordreset.php");
-        exit;
+        // Start output buffering
+        ob_start();
+
+        // Show the alert and redirect using JavaScript
+        echo '<script type="text/javascript">';
+        echo 'alert("For security reasons, you must change your password before accessing additional features.");';
+        echo 'window.location.href = "defaultpasswordreset.php";';
+        echo '</script>';
+
+        // End output buffering and send the output
+        ob_end_flush();
+
+        // Use exit to stop further script execution
+        exit();
     }
 }
 
