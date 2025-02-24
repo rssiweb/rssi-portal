4fdb39d3ebd5d9d6081bd21b16b53b7dff33cfa8
Somnath Saha <sahasomnath93@gmail.com>
2025-02-24 09:20:32 +0530

    ok

--- app/html/rssi-member/index.php
+++ app/html/rssi-member/index.php
@@ -126,6 +126,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 
 <body>
+<?php include 'banner.php'; ?>
     <main>
         <div class="container">
             <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
