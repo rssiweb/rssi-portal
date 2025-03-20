2a444b55c967813740e9a7d65e9c76e1ad24b2ac
Somnath Saha <sahasomnath93@gmail.com>
2025-03-20 16:23:13 +0530

    ok

---
+++ app/html/fetch_data_news.php
@@ -0,0 +1,39 @@
+<?php
+require_once __DIR__ . '/../bootstrap.php'; // Include your database connection file
+
+// Fetch latest 5 public notices
+$query_notices = "
+    SELECT noticeid, refnumber, date, subject, url 
+    FROM notice 
+    WHERE category = 'Public' 
+    ORDER BY date DESC 
+    LIMIT 5";
+$result_notices = pg_query($con, $query_notices);
+
+$notices = [];
+while ($row = pg_fetch_assoc($result_notices)) {
+    $notices[] = $row;
+}
+
+// Fetch latest 5 news items
+$query_news = "
+    SELECT noticeid, refnumber, date, subject, url 
+    FROM notice 
+    WHERE category = 'News & Press Releases' 
+    ORDER BY date DESC 
+    LIMIT 5";
+$result_news = pg_query($con, $query_news);
+
+$news = [];
+while ($row = pg_fetch_assoc($result_news)) {
+    $news[] = $row;
+}
+
+// Return data as JSON
+header('Content-Type: application/json');
+echo json_encode([
+    'success' => true,
+    'notices' => $notices,
+    'news' => $news
+]);
+?>
