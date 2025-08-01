40b3521ce3d0a3bbe49d2e5116e1fdce0cb6b295
Somnath Saha <sahasomnath93@gmail.com>
2025-08-01 07:46:19 +0530

    ok

--- app/html/rssi-member/fee_lock_management.php
+++ app/html/rssi-member/fee_lock_management.php
@@ -114,6 +114,7 @@ $months = [
     <title>Fee Collection Lock Management</title>
     <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <!-- Template Main CSS File -->
     <link href="../assets_new/css/style.css" rel="stylesheet">
