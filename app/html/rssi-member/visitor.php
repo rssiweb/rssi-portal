7fc8649ce206e84350eaed60aff7045cc9ee49ef
Somnath Saha <sahasomnath93@gmail.com>
2025-08-23 12:52:56 +0530

    ok

--- app/html/rssi-member/visitor.php
+++ app/html/rssi-member/visitor.php
@@ -133,7 +133,7 @@ if (!$phoneNumber) {
             policyLink: 'https://www.rssi.in/disvisiter'
         });
     </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <style>
         .col2 {
             display: inline-block;
