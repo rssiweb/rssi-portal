da08319383b149e38d6ba58f968a5ec7fbc14e31
Somnath Saha <sahasomnath93@gmail.com>
2025-12-29 10:23:55 +0530

    ok

--- app/html/rssi-member/survey.php
+++ app/html/rssi-member/survey.php
@@ -777,9 +777,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     </main><!-- End #main -->
 
     <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
-        integrity="sha384-AH1SsjB9g5JTYrtoDkVhY2Pn9HlXKG+C4fE9g6kfmHfAe8h+if3rpTkHidv+3wRK"
-        crossorigin="anonymous"></script>
+    <script
+        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+        crossorigin="anonymous">
+    </script>
     <!-- Vendor JS Files -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
