f57f332d62b1d4871ec110b89b5ab1add1fb53f1
Somnath Saha <sahasomnath93@gmail.com>
2025-12-30 00:19:01 +0530

    ok

--- app/html/rssi-member/gps.php
+++ app/html/rssi-member/gps.php
@@ -316,9 +316,8 @@ $resultArr = $result ? pg_fetch_all($result) : [];
     <script src="https://cdn.datatables.net/2.1.4/js/dataTables.js"></script>
     <script src="https://cdn.datatables.net/2.1.4/js/dataTables.bootstrap5.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
     <!-- Include Select2 JS -->
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 </head>
 
 <!-- =========================
