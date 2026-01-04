86ae8fcb98543bcf1584a6a822ceb82dde1fed4a
Somnath Saha <sahasomnath93@gmail.com>
2026-01-04 19:39:14 +0530

    ok

--- app/html/blog-api/google-auth/login.php
+++ app/html/blog-api/google-auth/login.php
@@ -1,11 +1,10 @@
 <?php
 require_once __DIR__ . '/config.php';
 
-// CORS headers - Add these at the very beginning
-header("Access-Control-Allow-Origin: https://www.rssi.in");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+// Add CORS headers if needed
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: ' . (isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '*'));
+header('Access-Control-Allow-Credentials: true');
 
 $data = json_decode(file_get_contents('php://input'), true);
 $idToken = $data['id_token'] ?? '';
