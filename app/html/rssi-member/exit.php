e032e30c82ac0175ea9fd0094e04f314005f2856
Somnath Saha <sahasomnath93@gmail.com>
2026-01-27 13:59:24 +0530

    ok

--- app/html/rssi-member/exit.php
+++ app/html/rssi-member/exit.php
@@ -173,15 +173,8 @@ if (@$_POST['form-type'] == "exit") {
     <main id="main" class="main">
 
         <div class="pagetitle">
-            <h1>Exit Process</h1>
-            <nav>
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="home.php">Home</a></li>
-                    <li class="breadcrumb-item"><a href="#">Work</a></li>
-                    <li class="breadcrumb-item"><a href="process-hub.php">Process Hub</a></li>
-                    <li class="breadcrumb-item active">Exit Process</li>
-                </ol>
-            </nav>
+            <h1><?php echo getPageTitle(); ?></h1>
+            <?php echo generateDynamicBreadcrumb(); ?>
         </div><!-- End Page Title -->
 
         <section class="section dashboard">
