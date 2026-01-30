60f70b8d8b880e4e35b1c7476e701e27dae8f619
Somnath Saha <sahasomnath93@gmail.com>
2026-01-30 19:10:13 +0530

    ok

--- app/html/rssi-member/api/donation.php
+++ app/html/rssi-member/api/donation.php
@@ -3,8 +3,6 @@ require_once __DIR__ . "/../../../bootstrap.php";
 
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
 
 include("../../../util/paytm-util.php");
 include("../../../util/email.php");
