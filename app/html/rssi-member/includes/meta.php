e123f31427bd42686658318493b9a2c96622abce
Somnath Saha <sahasomnath93@gmail.com>
2026-02-02 16:35:33 +0530

    ok_fixed_meta

--- app/html/rssi-member/includes/meta.php
+++ app/html/rssi-member/includes/meta.php
@@ -1,7 +1,8 @@
 <?php
 require_once __DIR__ . '../../includes/functions.php';
+?>
 
-echo '<title>' . htmlspecialchars(getPageTitle()) . ' - Phoenix Portal</title>';
-echo '<meta name="description" content="' . htmlspecialchars(getMetaDescription()) . '">';
-echo '<meta name="author" content="' . htmlspecialchars(getMetaAuthor()) . '">';
-echo '<meta name="robots" content="index, follow">';
+<title><?= htmlspecialchars(getPageTitle()) ?> - Phoenix Portal</title>
+<meta name="description" content="<?= htmlspecialchars(getMetaDescription()) ?>">
+<meta name="author" content="<?= htmlspecialchars(getMetaAuthor()) ?>">
+<meta name="robots" content="index, follow">
