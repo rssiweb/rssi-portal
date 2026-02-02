d7eb95ecaa89636c4191773e3f86a48fbf3b45a9
Somnath Saha <sahasomnath93@gmail.com>
2026-02-02 13:52:31 +0530

    ok

--- app/html/rssi-member/includes/meta.php
+++ app/html/rssi-member/includes/meta.php
@@ -2,12 +2,7 @@
 require_once __DIR__ . '../../includes/functions.php';
 ?>
 
-<title><?= htmlspecialchars(getPageTitle(), ENT_QUOTES, 'UTF-8') ?> - Phoenix Portal</title>
-
-<meta name="description"
-    content="<?= htmlspecialchars(getMetaDescription(), ENT_QUOTES, 'UTF-8') ?>">
-
-<meta name="author"
-    content="<?= htmlspecialchars(getMetaAuthor(), ENT_QUOTES, 'UTF-8') ?>">
-
+<title><?= getPageTitle() ?> - Phoenix Portal</title>
+<meta name="description" content="<?= getMetaDescription() ?>">
+<meta name="author" content="<?= getMetaAuthor() ?>">
 <meta name="robots" content="index, follow">
