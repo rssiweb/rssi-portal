9ec78d61c72f9ffba5b0e878a748bcbc82ada766
Somnath Saha <sahasomnath93@gmail.com>
2026-02-02 13:28:49 +0530

    ok

--- app/html/rssi-member/includes/meta.php
+++ app/html/rssi-member/includes/meta.php
@@ -2,7 +2,12 @@
 require_once __DIR__ . '../../includes/functions.php';
 ?>
 
-<title><?= htmlspecialchars(getPageTitle()) ?> - Phoenix Portal</title>
-<meta name="description" content="<?= htmlspecialchars(getMetaDescription()) ?>">
-<meta name="author" content="<?= htmlspecialchars(getMetaAuthor()) ?>">
+<title><?= htmlspecialchars(getPageTitle(), ENT_QUOTES, 'UTF-8') ?> - Phoenix Portal</title>
+
+<meta name="description"
+    content="<?= htmlspecialchars(getMetaDescription(), ENT_QUOTES, 'UTF-8') ?>">
+
+<meta name="author"
+    content="<?= htmlspecialchars(getMetaAuthor(), ENT_QUOTES, 'UTF-8') ?>">
+
 <meta name="robots" content="index, follow">
--- app/html/rssi-member/inventory-insights.php
+++ app/html/rssi-member/inventory-insights.php
@@ -38,7 +38,8 @@ rsort($academicYears);
 
 <head>
     <meta charset="UTF-8">
-    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include 'includes/meta.php' ?>
     <!-- Favicons -->
     <link href="../img/favicon.ico" rel="icon">
     <!-- Vendor CSS Files -->
@@ -206,8 +207,8 @@ rsort($academicYears);
     <!-- Vendor JS Files -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <!-- Template Main JS File -->
-      <script src="../assets_new/js/main.js"></script>
-  
+    <script src="../assets_new/js/main.js"></script>
+
 </body>
 
 </html>
