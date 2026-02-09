b1f117701c2027085310d01b0769fbce19ba19dd
Somnath Saha <sahasomnath93@gmail.com>
2026-02-09 14:30:05 +0530

    updated image compressor in reimb portal

--- app/html/rssi-member/reimbursement.php
+++ app/html/rssi-member/reimbursement.php
@@ -122,7 +122,7 @@ foreach ($accountNatures as $accountNature) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php include 'includes/meta.php' ?>
 
-    
+
 
     <!-- Favicons -->
     <link href="../img/favicon.ico" rel="icon">
@@ -272,7 +272,7 @@ foreach ($accountNatures as $accountNature) {
                                                             </span>
 
                                                             <span class="input-help">
-                                                                <input type="file" name="billdoc" class="form-control" required />
+                                                                <input type="file" name="billdoc" class="form-control" onchange="compressImageBeforeUpload(this)" required />
                                                                 <small id="passwordHelpBlock" class="form-text text-muted">Documents<span style="color:red">*</span></small>
                                                             </span>
 
@@ -337,8 +337,9 @@ foreach ($accountNatures as $accountNature) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
     <!-- Template Main JS File -->
-      <script src="../assets_new/js/main.js"></script>
-  
+    <script src="../assets_new/js/main.js"></script>
+    <script src="../assets_new/js/image-compressor-100kb.js"></script>
+
     <!-- Add this script at the end of the HTML body -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <!-- Bootstrap Modal -->
