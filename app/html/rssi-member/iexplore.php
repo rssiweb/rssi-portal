bb1f92840fdd378d9ee0395a998e340608658c92
Somnath Saha <sahasomnath93@gmail.com>
2026-02-13 10:14:32 +0530

    ok

--- app/html/rssi-member/iexplore.php
+++ app/html/rssi-member/iexplore.php
@@ -255,8 +255,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </script>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <?php include 'includes/meta.php' ?>
 
-    
     <meta content="" name="description">
     <meta content="" name="keywords">
 
@@ -701,8 +701,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
     <!-- Template Main JS File -->
-      <script src="../assets_new/js/main.js"></script>
-  
+    <script src="../assets_new/js/main.js"></script>
+
     <!-- Add this script at the end of the HTML body -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <!-- Bootstrap Modal -->
--- app/html/rssi-member/iexplore_admin.php
+++ app/html/rssi-member/iexplore_admin.php
@@ -56,7 +56,7 @@ if (!empty($courseid_search)) {
     </script>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    
+    <?php include 'includes/meta.php' ?>
     <meta content="" name="description">
     <meta content="" name="keywords">
     <!-- Favicons -->
@@ -257,8 +257,8 @@ if (!empty($courseid_search)) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
     <!-- Template Main JS File -->
-      <script src="../assets_new/js/main.js"></script>
-  
+    <script src="../assets_new/js/main.js"></script>
+
     <script>
         // JavaScript to handle dynamic addition and removal of study material fields
         document.getElementById('add-material').addEventListener('click', function() {
