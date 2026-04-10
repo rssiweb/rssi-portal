97d0eb49b3703b407b00ce24ff40d2abe63f3b4f
Somnath Saha <sahasomnath93@gmail.com>
2026-04-10 22:45:04 +0530

    ok

--- app/html/blog-api/get_blog_detail.php
+++ app/html/blog-api/get_blog_detail.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . "/../../bootstrap.php";
+require_once __DIR__ . "/../blog-api/google-auth/config.php";
 header('Content-Type: application/json');
 include(__DIR__ . "/../image_functions.php");
 
